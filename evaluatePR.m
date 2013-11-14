1f95246e23a8204be46ba15c04eb3448402dc69e
interxuxing <interxuxing.hust@gmail.com>
2013-11-14 16:26:00 +0900

    modify evaluatePR.m
    modify evaluatePR, add two types of evaluation: image-based and
    tag-based.

--- evaluatePR.m
+++ evaluatePR.m
@@ -1,4 +1,4 @@
-function [results]= evaluatePR(GTs, PREDs, topK)
+function [results]= evaluatePR(GTs, PREDs, topK, type)
 %
 % Usage: [results]= evaluatePR(GTs, PREDs, topK)
 %   results includes: prec, rec, f1, retrieved, f1Ind, precInd, recInd
@@ -6,12 +6,15 @@ function [results]= evaluatePR(GTs, PREDs, topK)
 %	GTs: K x n matrix containing the groundtruth (0, 1)
 %	PREDs: K x n matrix containing the prediction confidence (real values)
 %	topK: number of tags given to each image
+%   type: 'tag' / 'image'
 % Output: prec, rec, f1, N+, etc
 %
 
 
 GTs = (GTs>0);
-% compute precision, recall and N+ at top 5 annotations
+%% compute precision, recall and N+ at top 5 annotations
+
+% first select topK scores for each image
 hardPREDs = zeros(size(PREDs));
 for n = 1:size(GTs, 2)
         gt = GTs(:, n);
@@ -20,11 +23,27 @@ for n = 1:size(GTs, 2)
         si = si(1:topK);
 	hardPREDs(si, n) = 1;
 end
+
+if strcmp(type, 'tag')
+
 precInd = sum(hardPREDs.*GTs, 2)./max(sum(hardPREDs, 2), eps);
 prec = mean(precInd);
 
 recInd = sum(hardPREDs.*GTs, 2)./max(sum(GTs, 2), eps);
 rec = mean(recInd);
+
+elseif strcmp(type, 'image')
+
+precInd = sum(hardPREDs.*GTs, 1)./max(sum(hardPREDs, 1), eps);
+prec = mean(precInd);
+
+recInd = sum(hardPREDs.*GTs, 1)./max(sum(GTs, 1), eps);
+rec = mean(recInd);
+    
+else
+    fprintf('error type input! please set type as tag or image! \n');
+    return;
+end
 
 f1Ind = 2*precInd.*recInd./max(precInd+recInd, eps);
 f1 = 2*prec*rec/(prec+rec);
